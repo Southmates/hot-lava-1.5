7bc664972c6f1f56d77845294bbcea08c764b512
ismael <imolina218@gmail.com>
2024-01-04 15:00:36 -0300

    fix: videos pause when modal close

--- src/js/videomuting.js
+++ src/js/videomuting.js
@@ -15,11 +15,30 @@ function videoMuting() {
     videos[i].addEventListener("play", onPlay);
   }
 
+  // get all iframes in the scene
+  let allIframesOnScene = document.getElementsByTagName("iframe");
+
   let closebtn = document.querySelector(".js-close");
-    closebtn.addEventListener("click", function (e) { 
-    document.querySelectorAll("video").forEach((v) => {
+  closebtn.addEventListener("click", function (e) { 
+    /* document.querySelectorAll("video").forEach((v) => {
         v.pause();
-    });
+    }); */
+
+    for(var i = 0; i < allIframesOnScene.length; i++) {
+      // filtra elementos que no contengan player.vimeo.com en el src
+      if(allIframesOnScene[i].src == null || allIframesOnScene[i].src.indexOf("player.vimeo.com") == -1) {
+        continue;
+      }
+      
+      var iframe = allIframesOnScene[i];
+      var player = new Vimeo.Player(iframe);
+      
+          player.on('pause', function() {
+          console.log('paused');
+        });
+      
+      player.pause();
+    }
   });
 }
 
