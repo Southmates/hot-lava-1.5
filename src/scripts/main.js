0337f628aaed73d4c6e3b1d6adb4ebbfb31bcf64
Southmates <southmates.dev@gmail.com>
2025-11-11 19:09:08 -0300

    refactor: remove unused font files, clean up styles, and streamline component structure

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,6 @@ import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import Lenis from "@studio-freight/lenis";
 import customCursor from "./cursor.js";
 import videoMuting from "./videomuting.js";
-import Ukiyo from "ukiyojs";
 import "splitting/dist/splitting.css";
 import "splitting/dist/splitting-cells.css";
 import Splitting from "splitting";
@@ -144,256 +143,19 @@ function handleNav() {
     mobileNavOpen = false;
   });
 
-  const container = document.querySelector(".main");
-  const fadeHero = document.querySelector(".intro");
-  const about = document.querySelector(".about");
-  const howWeWork = document.querySelector(".how-we-work");
+  // const container = document.querySelector(".main");
+  // const fadeHero = document.querySelector(".intro");
+  // const about = document.querySelector(".about");
+  // const howWeWork = document.querySelector(".how-we-work");
   // const work = document.querySelector(".work");
-  const shop = document.querySelector(".shop");
-  const contact = document.querySelector(".contact");
-
-  const hero = document.querySelector(".hero");
-
-  ScrollTrigger.create({
-    trigger: hero,
-    start: "top top",
-    end: "bottom top",
-    onEnter: () =>
-      gsap.to(
-        container,
-        { backgroundColor: "#EE512F", duration: 1, ease: "ease.in" },
-        setBlankMenu()
-      ),
-    onEnterBack: () =>
-      gsap.to(
-        container,
-        { backgroundColor: "#EE512F", duration: 1, ease: "ease.in" },
-        setBlankMenu()
-      ),
-  });
-
-  ScrollTrigger.create({
-    trigger: fadeHero,
-    start: "top top",
-    end: "bottom +=75%",
-    // markers: true,
-    onEnter: () => {
-      gsap.to(
-        container,
-        { backgroundColor: "#EE512F", duration: 1, ease: "ease.in" },
-        setBlankMenu()
-      );
-    },
-    onLeave: () => {
-      gsap.to(
-        container,
-        { backgroundColor: "#108896", duration: 1, ease: "ease.out" },
-        setAboutMenu()
-      );
-
-      gsap.to(".team__card .team__blend", {
-        backgroundColor: "#108896",
-        duration: 1,
-        ease: "ease.in",
-      });
-    },
-    // onLeaveBack: () => gsap.to(container, {backgroundColor:"#EE512F", duration: 1, ease:"ease.in"}, setBlankMenu()),
-    onEnterBack: () =>
-      gsap.to(
-        container,
-        { backgroundColor: "#EE512F", duration: 1, ease: "ease.in" },
-        setBlankMenu()
-      ),
-  });
-
-  ScrollTrigger.create({
-    trigger: about,
-    start: "top +=75%",
-    end: "bottom top",
-    onEnter: () => {
-      gsap.to(
-        container,
-        { backgroundColor: "#108896", duration: 1, ease: "ease.in" },
-        setAboutMenu()
-      );
-      gsap.to(".team__card .team__blend", {
-        backgroundColor: "#108896",
-        duration: 1,
-        ease: "ease.in",
-      });
-    },
-    onLeave: () =>
-      gsap.to(
-        container,
-        { backgroundColor: "#1C374D", duration: 1, ease: "ease.out" },
-        setOurWayMenu()
-      ),
-    onEnterBack: () => {
-      gsap.to(
-        container,
-        { backgroundColor: "#108896", duration: 1, ease: "ease.in" },
-        setAboutMenu()
-      );
-    },
-  });
-
-  ScrollTrigger.create({
-    trigger: howWeWork,
-    start: "top 70%",
-    end: "bottom top",
-    onEnter: () => {
-      gsap.to(
-        container,
-        { backgroundColor: "#1C374D", duration: 1, ease: "ease.in" },
-        setOurWayMenu()
-      );
-      gsap.to(".team__card .team__blend", {
-        backgroundColor: "#1C374D",
-        duration: 1,
-        ease: "ease.in",
-      });
-    },
-    onLeave: () =>
-      gsap.to(
-        container,
-        { backgroundColor: "#1C374D", duration: 0.5, ease: "ease.out" },
-        setShopMenu()
-      ),
-    onLeaveBack: () => {
-      gsap.to(
-        container,
-        { backgroundColor: "#108896", duration: 1, ease: "ease.in" },
-        setAboutMenu()
-      );
-      gsap.to(".team__card .team__blend", {
-        backgroundColor: "#108896",
-        duration: 1,
-        ease: "ease.in",
-      });
-    },
-    onEnterBack: () =>
-      gsap.to(
-        container,
-        { backgroundColor: "#1C374D", duration: 1, ease: "ease.in" },
-        setOurWayMenu()
-      ),
-  });
-
-  // ScrollTrigger.create({
-  //   trigger: work,
-  //   start: "top 50%",
-  //   end: "bottom 20%",
-  //   onEnter: () =>
-  //     gsap.to(
-  //       container,
-  //       { backgroundColor: "#1C374D", duration: 1, ease: "ease.in" },
-  //       setWorkMenu()
-  //     ),
-  //   onLeave: () =>
-  //     gsap.to(
-  //       container,
-  //       { backgroundColor: "#E69C24", duration: 1, ease: "ease.out" },
-  //       setContactMenu()
-  //     ),
-  //   onEnterBack: () =>
-  //     gsap.to(
-  //       container,
-  //       { backgroundColor: "#1C374D", duration: 1, ease: "ease.in" },
-  //       setWorkMenu()
-  //     ),
-  // });
-
-  ScrollTrigger.create({
-    trigger: shop,
-    start: "top 50%",
-    end: "bottom 30%",
-    // markers: true,
-    onEnter: () =>
-      gsap.to(
-        container,
-        { backgroundColor: "#1C374D", duration: 1, ease: "ease.in" },
-        setShopMenu()
-      ),
-    onLeave: () =>
-      gsap.to(
-        container,
-        { backgroundColor: "#E69C24", duration: 1, ease: "ease.out" },
-        setContactMenu()
-      ),
-    onEnterBack: () =>
-      gsap.to(
-        container,
-        { backgroundColor: "#1C374D", duration: 1, ease: "ease.in" },
-        setShopMenu()
-      ),
-  });
-
-  ScrollTrigger.create({
-    trigger: contact,
-    start: "top 40%",
-    end: "bottom top",
-    // markers: true,
-    onEnter: () =>
-      gsap.to(
-        container,
-        { backgroundColor: "#E69C24", duration: 1, ease: "ease.in" },
-        setContactMenu()
-      ),
-    onLeave: () =>
-      gsap.to(
-        container,
-        { backgroundColor: "#1C374D", duration: 1, ease: "ease.out" },
-        setBlankMenu()
-      ),
-    onLeaveBack: () =>
-      gsap.to(
-        container,
-        { backgroundColor: "#1C374D", duration: 1, ease: "ease.out" },
-        setShopMenu()
-      ),
-  });
+  // const shop = document.querySelector(".shop");
+  // const contact = document.querySelector(".contact");
+  // const hero = document.querySelector(".hero");
+ 
 }
 
-// Scrolling "Team Members" horizontally
-function handleScrolHorizontal() {
-  const membersSection = document.querySelector(".team");
-  let sectionItems = gsap.utils.toArray(".team__card");
-
-  gsap.to(sectionItems, {
-    xPercent: -90 * (sectionItems.length - 1),
-    ease: "sine.out",
-    scrollTrigger: {
-      trigger: membersSection,
-      pin: true,
-      scrub: 2,
-      delay: 1,
-      // end: "+=100%" + membersSection.offsetWidth
-      end: "+=100%",
-      // end: "bottom",
-    },
-  });
-}
-
-var state = "";
-
-function checkMobile() {
-  var newState = state;
-  if (innerWidth > 1023) {
-    newState = "large";
-  } else {
-    newState = "mobile";
-  }
-
-  if (newState !== state) {
-    if (newState === "large") {
-      handleScrolHorizontal();
-    } else {
-      return;
-    }
-
-    state = newState;
-  }
-}
+ 
+ 
 
 // Handle modal, stop/start Lenis
 function handleModal() {
@@ -428,57 +190,8 @@ function handleModal() {
     lenis.start();
   }
 }
-
-// Handle swiper scroll
-document.addEventListener("resize", checkMobile);
-
-checkMobile();
-
-// Assign random slogan
-const sloganContainer = document.querySelector(".slogan p");
-let slogans = [
-  "*You’re going to like it here.",
-  "*Always non-toxic.",
-  "*Served fresh daily.",
-  "*Ideas may be hot to the touch.",
-  "*Jerk-free zone.",
-  "*Cannot be duplicated.",
-  "*There’s no limit.",
-  "*Creative discretion is not advised.",
-  "*Do not try this at home.",
-  "*We go with the flow.",
-];
-
-function randomSlogan() {
-  let randomValue;
-  randomValue = slogans[Math.floor(Math.random() * slogans.length)];
-
-  sloganContainer.append(randomValue);
-}
-
-// --> Function to set video source
-
-// const video = document.querySelector(".hero .wrapper video");
-// let videoSource = document.createElement("source");
-
-// videoSource.setAttribute("type", "video/mp4");
-
-// function handleVideoSource() {
-//   if (innerWidth > 480) {
-//     videoSource.setAttribute(
-//       "src",
-//       "https://player.vimeo.com/progressive_redirect/playback/891441750/rendition/1080p/file.mp4?loc=external&signature=fda31ffe3b21ba36b3a3aa3b65e4746b7a94eda1389a7ab609de05b7d73a8079"
-//     );
-//   } else {
-//     videoSource.setAttribute(
-//       "src",
-//       "https://player.vimeo.com/progressive_redirect/playback/891441750/rendition/540p/file.mp4?loc=external&signature=d93949a51886c059f163ef7f5780a6ce113c39ca9152aee94a4989038997c855"
-//     );
-//   }
-
-//   video.appendChild(videoSource);
-// }
-
+ 
+ 
 // ---> GSAP animations
 // Hero
 const logo = document.querySelector(".logo");
@@ -602,30 +315,7 @@ titles.forEach((title) => {
       },
     }
   );
-});
-
-// Bio images
-if (innerWidth >= 1024) {
-  bioImages.forEach((image) => {
-    gsap.fromTo(
-      image,
-      {
-        "will-change": "opacity",
-        x: +120,
-      },
-      {
-        ease: "none",
-        x: 0,
-        scrollTrigger: {
-          trigger: image,
-          start: "top bottom",
-          end: "center top",
-          scrub: true,
-        },
-      }
-    );
-  });
-}
+}); 
 
 // Copyright
 gsap.fromTo(
@@ -711,15 +401,6 @@ gsap.fromTo(
   }
 );
 
-// Images parallax
-const images_prllx = document.querySelectorAll(".parallax");
-
-images_prllx.forEach((image) => {
-  new Ukiyo(image, {
-    speed: 3,
-    scale: 1.1,
-  });
-});
 
 // Call functions
 handleNav();
@@ -739,10 +420,4 @@ customCursor();
 // Mute video when playing other or closing modal *
 videoMuting();
 
-gsap.to("#asterisk", {
-  duration: 3,
-  rotation: 360,
-  transformOrigin: "50% 50%",
-  ease: "power2.out",
-  repeat: -1,
-});
+ 
